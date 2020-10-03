3c270cf11e0acb8325e452de5adb99e89b038399
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-10-03 16:23:02 -0300

    abandonei aqui eu acho

--- twitter.py
+++ twitter.py
@@ -1,17 +1,19 @@
 import tweepy
+from datetime import datetime
 
 import authkeys
 auth = tweepy.OAuthHandler(authkeys.key1, authkeys.key2)
 auth.set_access_token(authkeys.key3, authkeys.key4)
 
-twiiter = tweepy.API(auth, wait_on_rate_limit=True)
+twiiter = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
 def getFirstTweets():
-    return twiiter.search(q='Google Pesquisar', result_type='recent', count=15)
+    return tweepy.Cursor(twiiter.search, 'Google Pesquisar').items(1)
 
 def getTweets(last_id):
-    return twiiter.search(q='Google Pesquisar', result_type='recent', since_id=last_id, count=30)
+    agora = datetime.now().strftime('%Y-%m-%d')
+    return tweepy.Cursor(twiiter.search, 'Google Pesquisar', since=agora).items(30)
 
 def reply(content, tweetId, url):
     twiiter.update_with_media(
