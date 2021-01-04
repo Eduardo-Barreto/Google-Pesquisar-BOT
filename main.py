693083a42c09a9e0d8e01d3731e2c3b336897c8e
Eduardo-Barreto <eduwagzozo@gmail.com>
2021-01-04 19:04:11 -0300

    try except so pra nao quebrar

--- main.py
+++ main.py
@@ -36,23 +36,26 @@ while True:
     sleep(5)
     clear()
     print('mais uma leva de tweets')
-    tweets = twitter.getTweets(last_id)
+    try:
+        tweets = twitter.getTweets(last_id)
 
-    for tweet in tweets:
-        if twitter.nao_foi(tweet.id):
-            content = text.formatText(tweet)
-            if 'google pesquisar' in content:
-                content = content.replace('google pesquisar', '')
-                if 'rt' not in content:
-                    if content != '' and content != ' ':
-                        print()
-                        print(content)
-                        link = text.getLink(content)
-                        print(link)
-                        screenshot.get(link)
-                        twitter.reply(tweet.id, content, link)
-                        last_tweets = open('./last_tweets.txt', 'a')
-                        last_tweets.write('\n' + str(tweet.id))
-                        last_tweets.close()
+        for tweet in tweets:
+            if twitter.nao_foi(tweet.id):
+                content = text.formatText(tweet)
+                if 'google pesquisar' in content:
+                    content = content.replace('google pesquisar', '')
+                    if 'rt' not in content:
+                        if content != '' and content != ' ':
+                            print()
+                            print(content)
+                            link = text.getLink(content)
+                            print(link)
+                            screenshot.get(link)
+                            twitter.reply(tweet.id, content, link)
+                            last_tweets = open('./last_tweets.txt', 'a')
+                            last_tweets.write('\n' + str(tweet.id))
+                            last_tweets.close()
 
-        last_id = tweet.id
+            last_id = tweet.id
+    except:
+        clear()
