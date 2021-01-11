17e54224ac71ab12fe49045bb1f718c004b12d57
Eduardo-Barreto <eduwagzozo@gmail.com>
2021-01-10 21:26:21 -0300

    filtro de texto refeito

--- text.py
+++ text.py
@@ -1,24 +1,25 @@
 from unicodedata import normalize
+from html import unescape
 import pyshorteners
 
+
 def formatText(tweet):
-    text = normalize('NFKD', tweet.text).encode('ASCII', 'ignore').decode('ASCII')
+    text = normalize('NFKD', tweet.text)
+    text = text.encode('ASCII', 'ignore')
+    text = text.decode('ASCII')
     text = text.lower()
-    text = text.replace('@', '')
-    text = text.replace('&gt;', '>')
-    text = text.replace('&lt;', '<')
+    text = unescape(text)
 
     text = text.strip()
-    text = text.replace('*', '')
-    text = text.replace('"', '')
-    text = text.replace(',', '')
-    text = text.replace('?', '')
-    text = text.replace('.', '')
-    text = text.replace(':', '')
-    text = text.replace('~', '')
-    text = text.replace('-', '')
-    text = text.replace('>', '')
-    text = text.replace('<', '')
+
+    to_replace = [
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+',
+        '=', '"', ',', '?', '.', ':', '~', '>', '<', '{', '}', ';'
+    ]
+
+    for item in to_replace:
+        text = text.replace(item, '')
+
     text = text.strip()
 
     if 'http' in text:
@@ -33,10 +34,11 @@ def formatText(tweet):
     text = text.strip()
     return text
 
+
 def getLink(search):
     search = search.replace(' ', '+')
 
-    url = 'https://www.google.com/search?&q='+ search+ '&ie=UTF-8&oe=UTF-8'
+    url = 'https://www.google.com/search?&q=' + search + '&ie=UTF-8&oe=UTF-8'
 
     shorted = pyshorteners.Shortener()
     url = shorted.tinyurl.short(url)
