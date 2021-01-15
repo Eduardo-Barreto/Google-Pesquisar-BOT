7d05a5d30fa7399a0348aedf15dbd3afcf849843
Eduardo-Barreto <eduwagzozo@gmail.com>
2021-01-15 20:11:37 -0300

    adequação PEP8

--- twitter.py
+++ twitter.py
@@ -4,28 +4,66 @@ from datetime import datetime
 import authkeys
 
 # Conta Principal
-auth = tweepy.OAuthHandler(authkeys.key1, authkeys.key2)
-auth.set_access_token(authkeys.key3, authkeys.key4)
-twiiter = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+auth = tweepy.OAuthHandler(
+    authkeys.key1,
+    authkeys.key2
+)
+auth.set_access_token(
+    authkeys.key3,
+    authkeys.key4
+)
+twiiter = tweepy.API(
+    auth,
+    wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True
+)
 
 # Conta Reserva
-auth_reserva = tweepy.OAuthHandler(authkeys.reserva_key1, authkeys.reserva_key2)
-auth_reserva.set_access_token(authkeys.reserva_key3, authkeys.reserva_key4)
-reserva = tweepy.API(auth_reserva, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+auth_reserva = tweepy.OAuthHandler(
+    authkeys.reserva_key1,
+    authkeys.reserva_key2
+)
+auth_reserva.set_access_token(
+    authkeys.reserva_key3,
+    authkeys.reserva_key4
+)
+reserva = tweepy.API(
+    auth_reserva,
+    wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True
+)
 
 # Conta Reserva
-auth_reserva2 = tweepy.OAuthHandler(authkeys.reserva2_key1, authkeys.reserva2_key2)
-auth_reserva2.set_access_token(authkeys.reserva2_key3, authkeys.reserva2_key4)
-reserva2 = tweepy.API(auth_reserva2, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+auth_reserva2 = tweepy.OAuthHandler(
+    authkeys.reserva2_key1,
+    authkeys.reserva2_key2
+)
+auth_reserva2.set_access_token(
+    authkeys.reserva2_key3,
+    authkeys.reserva2_key4
+)
+reserva2 = tweepy.API(
+    auth_reserva2,
+    wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True
+)
 
 
 def getFirstTweet():
     agora = datetime.now().strftime('%Y-%m-%d')
-    return tweepy.Cursor(twiiter.search, q='google pesquisar', since=agora).items(1)
+    return tweepy.Cursor(
+        twiiter.search,
+        q='google pesquisar',
+        since=agora
+    ).items(1)
 
 
 def getTweets(last_id):
-    return tweepy.Cursor(twiiter.search, q='google pesquisar', since_id=last_id).items(15)
+    return tweepy.Cursor(
+        twiiter.search,
+        q='google pesquisar',
+        since_id=last_id
+    ).items(15)
 
 
 def reply(tweet_id, content, url):
